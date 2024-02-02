1b23557d471bfbd92a4bd2e8ff478877a474d5b5
Tholeti Harsha Vardhan <106816042+harsha-vardhan-tholeti@users.noreply.github.com>
2024-02-02 07:04:10 +0530

    global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 // Importing Routes
 import userRoutes from "./routes/user.js";
 import { connectDB } from "./utils/features.js";
+import { errorMiddleware } from "./middlewares/error.js";
 
 const port = 3000;
 
@@ -14,6 +15,8 @@ app.use(express.json());
 
 // Using Routes
 app.use("/api/v1/user", userRoutes);
+
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is working on http://localhost:${port}`);
